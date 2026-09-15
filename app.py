bf881e9427fa3c15c6f98361ff024452cd15fce4
Laxmi <laxmi916@gmail.com>
2026-09-15 22:57:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if "chat_session" not in st.session_state:
         automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=True)
     )
     st.session_state.chat_session = st.session_state.client.chats.create(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         config=config
     )
 
